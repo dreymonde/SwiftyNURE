f986495da373c76a96722c5c6a028c6168849285
Oleg Dreyman <dreymonde@me.com>
2016-03-19 18:47:32 +0200

    GroupsCISTParser.imperativeParse remove.

--- SwiftyNURE/GroupsParser.swift
+++ SwiftyNURE/GroupsParser.swift
@@ -7,34 +7,6 @@
 //
 
 struct GroupsCISTParser: JSONCISTParser {
-
-    /// Deprecated
-    static func imperativeParse(fromJSON json: JSON) -> [Group]? {
-        var groups = [Group]()
-        guard let university = json["university"] as? JSON,
-            faculties = university["faculties"] as? [JSON] else { return nil }
-        for faculty in faculties {
-            if let directions = faculty["directions"] as? [JSON] {
-                for direction in directions {
-                    // Groups
-                    if let jGroups = direction["groups"] as? [JSON] {
-                        groups.appendContentsOf(GroupsCISTParser.groups(fromJSONArray: jGroups, existingGroups: groups))
-                    }
-
-                    // Specializations
-                    if let specialities = direction["specialities"] as? [JSON] {
-                        for speciality in specialities {
-                            if let jGroups = speciality["groups"] as? [JSON] {
-                                groups.appendContentsOf(GroupsCISTParser.groups(fromJSONArray: jGroups, existingGroups: groups))
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return groups
-
-    }
     
     static func parse(fromJSON json: JSON) -> [Group]? {
         guard let university = json["university"] as? JSON,
